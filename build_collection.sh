ff3c2dc6c998ad4cab37018230631a589cc442aa
Jeffrey van Pelt <jeff@vanpelt.one>
2025-06-30 18:34:34 +0200

    update script

--- build_collection.sh
+++ build_collection.sh
@@ -38,6 +38,7 @@ echo "| ---------------------- | ------- |" >> $VERSION_FILE
 
 while read ROLE_NAME ROLE_URL
 do
+  ROLE_NAME=$(echo $i | cut -d\" -f2 | cut -d- -f2)
   echo "Processing role $ROLE_NAME"
   git clone $ROLE_URL $START_DIR/roles/$ROLE_NAME >/dev/null 2>&1
   cd $START_DIR/roles/$ROLE_NAME || exit 1
